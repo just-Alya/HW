dfe1769a0699195b6dfcca3c689a1b2c64e41fe7
Alia <ignatenkoaleksandra12@gmail.com>
2026-04-23 19:20:04 +0300

    структури задача 2

--- hone2.cpp
+++ hone2.cpp
@@ -1,14 +1,25 @@
-#include<iostream>
-int main()
-{
-int a;
-std::cin>>a;
-if(a<0)
-{
-std::cout<<"number -";
-}
-if(a>0)
-{
-std::cout<<"number +";
-}
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct Product {
+    string name;
+    double price;
+    int quantity;
+};
+
+int main() {
+    Product p;
+    cout << "Enter name: ";
+    getline(cin, p.name);
+    cout << "Enter price: ";
+    cin >> p.price;
+    cout << "Enter quantity: ";
+    cin >> p.quantity;
+
+    cout << fixed << setprecision(2);
+    cout << "Total: " << p.price * p.quantity << " UAH" << endl;
+    return 0;
 }
